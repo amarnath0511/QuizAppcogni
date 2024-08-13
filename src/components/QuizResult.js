480144ac75e384233551eb9a617afa86c7811a4f
Natarajan <2354032@cognizant.com>
2024-08-13 18:12:24 +0530

    Pie chart added in the result

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+}from 'chart.js';
+
+import {Pie} from "react-chartjs-2";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend
+);
 
 function QuizResult(props) {
+
+  const data={
+    labels : [' Your Score', 'Total Score'],
+    datasets: [
+      {
+        data: [props.score, (props.totalScore - props.score)],
+        backgroundColor: ['Green', 'red']
+      }
+    ]
+  };
+  const options ={
+
+  }
   return (
     <>
     <div className='show-score'>
@@ -8,7 +35,27 @@ function QuizResult(props) {
         Total Score:{props.totalScore}
     </div>
     <button id="next-button" onClick={props.tryAgain}>Try Again</button>
+    
+    <div
+       style={
+        {
+          padding: '30px',
+          width: "50%",    position: "absolute",
+          top: "0"
+        }
+       }
+    >
+      <Pie
+        data= {data}
+        options = {options}
+      >
+
+      </Pie>
+    </div>
+
     </>
+   
+    
   )
 }
 
